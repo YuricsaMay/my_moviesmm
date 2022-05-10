6cb3358220a4292bd9060bd628dfef88635f785d
YuricsaMay <75384236+YuricsaMay@users.noreply.github.com>
2022-05-09 21:10:34 -0500

    layout

--- app/src/main/java/com/example/mymoviesmariamay/MainActivity.kt
+++ app/src/main/java/com/example/mymoviesmariamay/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.mymoviesmariamay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymoviesmariamay.ui.theme.MyMoviesMariaMayTheme
@@ -30,14 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String,modifier:Modifier = Modifier) {
+    Text(text = "Hello $name!",modifier = modifier)
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name="Android Greeting",
+widthDp=400,
+heightDp=200
+)
 @Composable
 fun DefaultPreview() {
     MyMoviesMariaMayTheme {
-        Greeting("Android")
+        Box(
+            modifier= Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Greeting("Maria")
+            Greeting(name="Android", modifier=Modifier.align(Alignment.BottomEnd))
+        }
+
     }
 }
